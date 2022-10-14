322203b6d267adcbb50c4ef64049b872d96b9d74
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-14 10:57:51 -0400

    remove print

--- fuzz/fuzz_targets/try_compression.rs
+++ fuzz/fuzz_targets/try_compression.rs
@@ -3,6 +3,5 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let s = String::from_utf8_lossy(data);
-    println!("Fuzz: {}", s);
     let _ = atm::directives::gen::try_compression_from_str(&s);
 });
